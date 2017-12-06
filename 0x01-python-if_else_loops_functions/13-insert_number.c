9a3d47f2806d71adf1e095e0bfdf246addd3b0ef
KevinClauson <kevinclauson99@gmail.com>
2017-12-06 03:41:08 +0000

    null check

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,6 +7,8 @@ listint_t *insert_node(listint_t **head, int number)
 	if (head == NULL)
 		return NULL;
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = number;
 	if (*head == NULL)
 		return (new);
