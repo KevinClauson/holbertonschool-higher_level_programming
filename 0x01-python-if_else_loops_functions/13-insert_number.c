49cce381c9215b332208340fd626fbde17093f30
KevinClauson <kevinclauson99@gmail.com>
2017-12-06 04:05:55 +0000

    change logic

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -21,22 +21,10 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new);
 	}
 	current_p = next_p = *head;
-	next_p = next_p->next;
-	if (current_p->n > number)
-	{
-		*head = new;
-		new->next = next_p;
-		return (new);
-	}
-	if (new->n < next_p->n)
-	{
-		current_p->next = new;
-		new->next = next_p;
-		return (new);
-	}
+
 	while(next_p->n < number && next_p)
 	{
-		current_p = current_p->next;
+		current_p = next_p;
 		next_p = next_p->next;
 	}
 	current_p->next = new;
