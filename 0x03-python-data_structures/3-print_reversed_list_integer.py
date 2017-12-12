94a9b7384855f4567dc437201d485c3b6045af50
KevinClauson <kevinclauson99@gmail.com>
2017-12-12 23:50:30 +0000

    check for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,10 +2,11 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    j = 0
-    for i in my_list:
-        j += 1
-    j -= 1
-    for i in my_list:
-        print("{:d}".format(my_list[j]))
+    if my_list:
+        j = 0
+        for i in my_list:
+            j += 1
         j -= 1
+        for i in my_list:
+            print("{:d}".format(my_list[j]))
+            j -= 1
