3f56cea69406453628a630de85ee7a3a027d2e36
KevinClauson <kevinclauson99@gmail.com>
2017-12-13 07:16:42 +0000

    add comments

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * length - gets length of linked list.
+ * @head: head of linked list
+ * Return: the length of the list.
+ */
 int length(listint_t *head)
 {
 	int i;
@@ -12,7 +16,11 @@ int length(listint_t *head)
 	}
 	return (i);
 }
-
+/**
+ * is_palindrome - checks if linked list is palidrome.
+ * @head: pointer to head of linked list
+ * Return: 1 if it is palindrome and 0 if not.
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *start, *check;
@@ -26,7 +34,7 @@ int is_palindrome(listint_t **head)
 		return (1);
 	half = len / 2;
 
-	for(i = 0; i < half; ++i)
+	for (i = 0; i < half; ++i)
 	{
 		check = *head;
 		for (j = 0; j < len - 1; ++j)
