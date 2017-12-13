7927ff599c9c66d9d68ee94c9bcbfe40177afbcb
KevinClauson <kevinclauson99@gmail.com>
2017-12-13 05:50:23 +0000

    take out semi colon

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -8,7 +8,7 @@ def delete_at(my_list=[], idx=0):
         j = 0
         while j < len(my_list):
             if j == idx:
-                my_list.remove(my_list[j]);
+                my_list.remove(my_list[j])
             j += 1
         return my_list
     return my_list
