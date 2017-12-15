4f3f3ce5b90c77cbc1a0e8b000bd8d67cc6fc0eb
KevinClauson <kevinclauson99@gmail.com>
2017-12-15 06:38:55 +0000

    cover when no string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-    digit = [1, 5, 10, 50, 100, 500, 1000]
-    num = []
-    k = 0
-    for i in roman_string:
+    if roman_string:
+        roman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+        digit = [1, 5, 10, 50, 100, 500, 1000]
+        num = []
         k = 0
-        for j in roman:
-            if i == j:
-                num.append(digit[k])
-            k += 1
-    length = len(num)
-    k = length - 1
-    result = 0
-    while k >= 0:
-        if k > 0 and num[k] > num[k-1]:
-            result = num[k] - num[k-1]
+        for i in roman_string:
+            k = 0
+            for j in roman:
+                if i == j:
+                    num.append(digit[k])
+                k += 1
+        length = len(num)
+        k = length - 1
+        result = 0
+        while k >= 0:
+            if k > 0 and num[k] > num[k-1]:
+                result = num[k] - num[k-1]
+                k -= 1
+            else:
+                result = result + num[k]
             k -= 1
-        else:
-            result = result + num[k]
-        k -= 1
-    return result
+        return result
+    else:
+        return 0
