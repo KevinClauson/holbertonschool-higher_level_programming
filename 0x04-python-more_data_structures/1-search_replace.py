75321189e341bcc3edb8cf9b5c7e4560af174aac
KevinClauson <kevinclauson99@gmail.com>
2017-12-15 00:00:34 +0000

    edit indent

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,10 @@
 
 
 def search_replace(my_list, search, replace):
-     my_matrix = []
-     for i in my_list:
-         if i == search:
-             my_matrix.append(replace)
-         else:
-             my_matrix.append(i)
-     return my_matrix
+    my_matrix = []
+    for i in my_list:
+        if i == search:
+            my_matrix.append(replace)
+        else:
+            my_matrix.append(i)
+    return my_matrix
