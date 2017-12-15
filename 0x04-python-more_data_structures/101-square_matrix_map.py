48e73fdd75571b2cc6c98e32f91b9d71c1039a77
KevinClauson <kevinclauson99@gmail.com>
2017-12-15 07:23:49 +0000

    delete a line

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    my_matrix = [list(map(lambda x: x*x, x)) for x in matrix]
-    return my_matrix
+    return [list(map(lambda x: x*x, x)) for x in matrix]
