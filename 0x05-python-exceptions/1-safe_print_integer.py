980b365fdaa22b9474f90530b892432bb6c7e631
KevinClauson <kevinclauson99@gmail.com>
2018-01-02 23:19:01 +0000

    all files

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+            num = value + 1
+            print("{:d}".format(value))
+            return True
+    except:
+        return False
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            q = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            q = 0
+            print("division by 0")
+        except TypeError:
+            q = 0
+            print("wrong type")
+        except IndexError:
+            q = 0
+            print("out of range")
+        finally:
+            new_list.append(q)
+    return new_list
