f047dcee1949254c2c8a5a99fefb4af96abd1389
KevinClauson <kevinclauson99@gmail.com>
2018-01-04 05:22:00 +0000

    comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Square Module """
 
+
 class Square:
+    """ Square Class """
     def __init__(self, size):
+        """ instantization of size """
         self.__size = size
-        
