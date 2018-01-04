915f2fb88d7265cbfde61c090138cfe33867e2e6
KevinClauson <kevinclauson99@gmail.com>
2018-01-04 05:09:09 +0000

    add comments

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+""" Square Module """
 
 
 class Square:
+    """ Square Class """
     def __init__(self, size=0):
+        """ instantization of size """
         self.size = size
 
-    @property    
+    @property
     def size(self):
+        """ returns the size var"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ sets the size variable checking if int or less than 0"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -19,9 +24,11 @@ class Square:
             self.__size = value
 
     def area(self):
+        """ returns the squared area of square"""
         return self.__size ** 2
 
     def my_print(self):
+        """prints the square """
         if self.__size == 0:
             print()
         else:
