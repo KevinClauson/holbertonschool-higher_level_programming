b06413ac586c31a7676bacdb7a02201d22cc944c
KevinClauson <kevinclauson99@gmail.com>
2018-01-18 06:04:25 +0000

    take out white space

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,7 @@ def read_lines(filename="", nb_lines=0):
     line_count = 0
     with open(filename, mode='r', encoding='utf-8') as f:
         if nb_lines <= 0:
-            for line in f: 
+            for line in f:
                 print("{}".format(line.rstrip()))
         else:
             for line in f:
