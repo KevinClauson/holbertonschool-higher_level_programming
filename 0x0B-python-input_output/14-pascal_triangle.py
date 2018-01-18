94421950c64e33cb3e73f244a5fe4a6187ccbfe1
KevinClauson <kevinclauson99@gmail.com>
2018-01-18 14:58:49 +0000

    did pascals

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    """build pascals triangle with list of lists"""
+    new = []
+    if n <= 0:
+        return new
+    t = f = 0
+    new = [[1]]
+    if n == 1:
+        return new
+    while t < (n - 1):
+        temp = []
+        for i in range(len(new[t])):
+            if i == 0:
+                temp.append(new[t][i])
+            if i < (len(new[t]) - 1):
+                temp.append(new[t][i] + new[t][i+1])
+            if i == (len(new[t]) - 1):
+                temp.append(new[t][i])
+        t+=1
+        new.append(temp)
+    return new
