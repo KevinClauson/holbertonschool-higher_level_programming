8ff287fab25fa2abf0b63eaa87d0ff461b77b143
KevinClauson <kevinclauson99@gmail.com>
2018-01-25 02:29:56 +0000

    style edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,7 +114,7 @@ class Rectangle(Base):
         pretty print of rectangle
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                    self.width, self.height)
+                                                       self.width, self.height)
 
     def update(self, *args, **kwargs):
         """
