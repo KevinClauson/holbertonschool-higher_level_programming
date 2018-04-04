77c8a5b0aa53610c16fe0bcf332b7e112c2d05cc
KevinClauson <kevinclauson99@gmail.com>
2018-04-04 07:30:24 +0000

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -18,15 +18,16 @@ def find_peak(list_of_integers):
     start = 0
     end = size - 1
     while start < end:
-        if mid != 0 and mid != size - 1 and nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+        if mid == 0 or mid == size - 1:
+            return nums[mid]
+        elif nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
             return nums[mid]
         elif nums[mid] < nums[mid+1]:
             start = mid
             mid = (start + end) // 2
-        elif mid != 0 and mid != size - 1:
-            end = start
-            mid = start + end // 2
-        else:
-            start = mid
+        elif nums[mid] < nums[mid-1]:
+            end = mid
             mid = (start + end) // 2
+        else:
+            return nums[mid]
     return nums[mid]
