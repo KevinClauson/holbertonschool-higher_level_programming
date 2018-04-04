76bb18da6cdd05dcace4d32aed753f3edcc0df1a
KevinClauson <kevinclauson99@gmail.com>
2018-04-04 06:49:09 +0000

    add thins

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,14 +10,14 @@ def find_peak(list_of_integers):
     size = len(nums)
     if not nums or size == 0:
         return None
-    elif num == 1:
+    elif size == 1:
         return nums[0]
     peak = None
     for i in range(size):
         if i == 0 and nums[i] >= nums[i+1]:
             peak = nums[i]
         elif i == size - 1 and nums[i] > nums[i-1]:
-            return list_of_integers[i]
+            return nums[i]
         elif i != 0 and i != size - 1:
             if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
                 return nums[i]
