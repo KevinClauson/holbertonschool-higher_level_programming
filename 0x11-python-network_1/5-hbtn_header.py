4684e3dba570292826a031aa267f9958540d2ec1
KevinClauson <kevinclauson99@gmail.com>
2018-04-09 21:38:00 +0000

    add file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that fetches URL prints out header
+"""
+
+import sys
+import requests
+
+
+def get_url(URL):
+    """
+    prints out header value
+    """
+    r = requests.get(URL)
+    print("{}".format(r.headers['X-Request-Id'])
+
+if __name__ == "__main__":
+    get_url(sys.argv[1])
