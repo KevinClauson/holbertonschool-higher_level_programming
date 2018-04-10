c561f504e5f85374a9f78d44e2913cfa4ae9e56e
KevinClauson <kevinclauson99@gmail.com>
2018-04-10 19:45:07 +0000

    style

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -16,22 +16,23 @@ def get_url(url, name):
         my_json = r.json()
         num_result = my_json.get('count')
         print("Number of results: {}".format(num_result))
-        if num_result > 0:
-                while True:
-                        next = my_json.get('next')
-                        names = my_json.get('results')
-                        for i in names:
-                                print(i.get('name'))
-                                films = (i.get('films'))
-                                for f in films:
-                                        rec = requests.get(f)
-                                        film_j = rec.json()
-                                        print("\t{}".format(film_j.get('title')))
-                        if next is None:
-                                break
-                        else:
-                                r = requests.get(next)
-                                my_json = r.json()
+        if num_result == 0:
+                return
+        while True:
+                next = my_json.get('next')
+                names = my_json.get('results')
+                for i in names:
+                        print(i.get('name'))
+                        films = (i.get('films'))
+                        for f in films:
+                                rec = requests.get(f)
+                                j = rec.json()
+                                print("\t{}".format(j.get('title')))
+                if next is None:
+                        break
+                else:
+                        r = requests.get(next)
+                        my_json = r.json()
 
 if __name__ == "__main__":
         url = 'https://swapi.co/api/people/'
