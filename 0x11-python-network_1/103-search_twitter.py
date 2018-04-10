8467323cb9e6fd030668cda01ff87efa02cac871
KevinClauson <kevinclauson99@gmail.com>
2018-04-10 21:50:33 +0000

    add twitter

--- 0x11-python-network_1/103-search_twitter.py
+++ 0x11-python-network_1/103-search_twitter.py
@@ -5,6 +5,7 @@ Python script that sends GET search to twitter api
 
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 import base64
 
 
@@ -12,31 +13,34 @@ def get_url(url, api, secret, search):
         """
         prints out header value
         """
-        payload = {'search': name, 'include': 'metadata'}
-        r = requests.get(url, params=payload)
+        set_of = api + ':' + secret
+        my_data = base64.b64encode(set_of.encode('ascii'))
+        header = {"Authorization": "Basic {}".format(my_data.decode()),
+                  "Content-Type": "application/x-www-form-urlencoded;\
+                  charset=UTF-8"}
+        body = {"grant_type": "client_credentials"}
+        r = requests.post(url, headers=header, data=body)
         my_json = r.json()
-        num_result = my_json.get('count')
-        print("Number of results: {}".format(num_result))
-        if num_result == 0:
-                return
-        while True:
-                next = my_json.get('next')
-                names = my_json.get('results')
-                for i in names:
-                        print(i.get('name'))
-                        films = (i.get('films'))
-                        for f in films:
-                                rec = requests.get(f)
-                                j = rec.json()
-                                print("\t{}".format(j.get('title')))
-                if next is None:
+        token = my_json.get("access_token")
+        new_header = {"Authorization": "Bearer {}".format(token)}
+        new_url = "https://api.twitter.com/1.1/search/tweets.json"
+        payload = {'q': search}
+        re = requests.get(new_url, headers=new_header, params=payload)
+        new_json = re.json()
+        final_json = new_json.get('statuses')
+        cnt = 0
+        for tweet in final_json:
+                tweet_id = tweet.get("id")
+                tweet_text = tweet.get("text")
+                user = tweet.get("user")
+                tweet_owner = user.get('name')
+                print("[{}] {} {}".format(tweet_id, tweet_text, tweet_owner))
+                cnt += 1
+                if cnt == 5:
                         break
-                else:
-                        r = requests.get(next)
-                        my_json = r.json()
 
 if __name__ == "__main__":
-        url = 'https://swapi.co/api/people/'
+        url = 'https://api.twitter.com/oauth2/token'
         api = sys.argv[1]
         secret = sys.argv[2]
         search = sys.argv[3]
