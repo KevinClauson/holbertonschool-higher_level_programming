d7c384dca0c31d3e0f73aaa0764be203e227ff44
KevinClauson <kevinclauson99@gmail.com>
2018-04-10 16:20:12 +0000

    add limit to 10 listings

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,12 +14,16 @@ def get_url(url, repo, owner):
         r = requests.get(url)
         my_json = r.json()
         my_list = []
+        cnt = 0
         for rec in my_json:
                 sha = rec.get('sha')
                 commit_dic = rec.get("commit")
                 author = commit_dic.get("author")
                 name = author.get("name")
                 print("{} {}".format(sha, name))
+                cnt += 1
+                if cnt == 10:
+                        break
 
 if __name__ == "__main__":
         url = 'https://api.github.com/repos/'
