81c91467221332c53a2db798d9f759aa905fed1d
KevinClauson <kevinclauson99@gmail.com>
2018-04-10 19:39:25 +0000

    add titles of films

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Python script that sends POST request
+"""
+
+import sys
+import requests
+
+
+def get_url(url, name):
+        """
+        prints out header value
+        """
+        payload = {'search': name, 'include': 'metadata'}
+        r = requests.get(url, params=payload)
+        my_json = r.json()
+        num_result = my_json.get('count')
+        print("Number of results: {}".format(num_result))
+        if num_result > 0:
+                while True:
+                        next = my_json.get('next')
+                        names = my_json.get('results')
+                        for i in names:
+                                print(i.get('name'))
+                                films = (i.get('films'))
+                                for f in films:
+                                        rec = requests.get(f)
+                                        film_j = rec.json()
+                                        print("\t{}".format(film_j.get('title')))
+                        if next is None:
+                                break
+                        else:
+                                r = requests.get(next)
+                                my_json = r.json()
+
+if __name__ == "__main__":
+        url = 'https://swapi.co/api/people/'
+        name = sys.argv[1]
+        get_url(url, name)
