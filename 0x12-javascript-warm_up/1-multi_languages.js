8f72d858425544b4a31f8c4f4aa1488a694fba9d
KevinClauson <kevinclauson99@gmail.com>
2018-05-03 06:13:07 +0000

    add file

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* js c and python are sweet */
+const myVar = ['C is fun', 'Python is cool',
+  'Javascript is amazing'
+];
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
+}
