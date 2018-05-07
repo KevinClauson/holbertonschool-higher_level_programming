722cf0de8262b5a167df79b0d25ba6cd5393ae86
KevinClauson <kevinclauson99@gmail.com>
2018-05-07 04:47:10 +0000

    add text to printout

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ if (process.argv[2]) {
   if (isNaN(process.argv[2])) {
     console.log('Not a number');
   } else {
-    console.log(Math.trunc(process.argv[2]));
+    console.log('My number: ' + Math.trunc(process.argv[2]));
   }
 } else {
   console.log('Not a number');
