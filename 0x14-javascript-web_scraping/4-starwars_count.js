990e7080f8e60e12dbc5e140d3e1a2b727dfe044
KevinClauson <kevinclauson99@gmail.com>
2018-05-09 05:04:47 +0000

    add check to 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
 /* read and prints contents of a file */
 
-let url = 'https://swapi.co/api/people/18/';
+let url;
 let request = require('request');
+let incomingUrl = process.argv[2];
+if (incomingUrl == 'http://swapi.co/api/films') {
+  url = 'https://swapi.co/api/people/18/';
+} else {
+  url = incomingUrl;
+}
 
 request.get(url, function getUrl (error, response, body) {
   if (error) console.log(error);
-  else {
+  else if (response && body) {
     let bodyParse = JSON.parse(body);
     console.log(bodyParse['films'].length);
   }
