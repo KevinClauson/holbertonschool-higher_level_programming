7c23d62004ccd738362fe617c81c7df1526c6a35
KevinClauson <kevinclauson99@gmail.com>
2018-05-10 05:00:43 +0000

    add 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/* makes a rectangle class */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let myStr = Array(this.width + 1).join("X");
+    for (let i = 0; i < this.height; i++) {
+      console.log(myStr);
+    }
+  }
+}
+
+module.exports = Rectangle;
