8737becb4b709624b782963d41200e47c3247ae6
KevinClauson <kevinclauson99@gmail.com>
2018-05-10 06:03:06 +0000

    add 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* returns the number of occurrences in a list */
+
+exports.nbOccurences = function (list, searchElement) {
+  let cnt = 0;
+  for (let i = 0; i < list.length; ++i) {
+    if (list[i] === searchElement) cnt++;
+  }
+  return cnt;
+};
