2b16e58cc1b99f2e7210862835ca9c4c720f049c
KevinClauson <kevinclauson99@gmail.com>
2018-05-10 04:09:42 +0000

    changed style

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,9 +2,9 @@
 /* makes a rectangle class */
 
 class Rectangle {
-    constructor(w, h) {
-    this.width = w;
-    this.height = h;
+  constructor (w, h) {
+  this.width = w;
+  this.height = h;
   }
 }
 
